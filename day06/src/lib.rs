882e9a61c8b677450d7da6d936a0fe4e33d5f8e7
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2024-12-06 23:11:22 +0100

    day06: wip part 2 approach 1
    Call me Edison, because I've found one more way not to make a lightbulb.
    
    My approach here works perfectly for the example, but it can't forward-
    project when an obstacle now will form a loop with paths that it has
    not yet created. And it's pointless to have the projection turn the corner
    and keep going, because it fundamentally cannot tell where the normal
    path will have reached.
    
    Another intractable approach: work backwards from the end of the map
    to see if we can generate a loop in that direction, backwards. Problem:
    the search space doubles literally every step of the way.
    
    Another intractable approach: run the guard's movements, then reset
    to the initial position and use this current approach to see if inserting
    an obstacle generates an eventual loop. The problem here is that this
    would be extremely likely to create false loops by intersecting with
    a future path segment which paradoxically fails to become part of the
    paths, due to the insertion of this obstacle.
    
    I think the next approach has to be to just brute-force it, checking
    whether the insertion of a new obstacle in every single point ends
    up producing an infinite loop.

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,5 +1,6 @@
 use aoclib::geometry::{tile::DisplayWidth, Direction, MapConversionErr, Point};
 use std::{
+    collections::HashSet,
     ops::{Index, IndexMut},
     path::Path,
 };
@@ -112,12 +113,60 @@ pub fn part1(input: &Path) -> Result<(), Error> {
     Ok(())
 }
 
+/// `true` when an immediate right turn from this position would encounter a path we have previously visited
+fn obstacle_ahead_succeeds(guard: &Guard, map: &Map, visited: &Visited) -> bool {
+    let orientation = guard.orientation.turn_right();
+    let (dx, dy) = orientation.deltas();
+    for point in map.project(guard.position, dx, dy) {
+        if map[point] == Tile::Obstruction {
+            return false;
+        }
+        if visited[point].is_set(orientation) {
+            return true;
+        }
+    }
+    false
+}
+
+// too low: 413
+// too low: 416
 pub fn part2(input: &Path) -> Result<(), Error> {
     // plan: keep track of previously visited, not just boolean, but a stack of directions
     // at each position, project what would happen if we turned right, right now
     // if we encounter a place where we'd be proceeding along the current dirction before hitting
     // an obstacle or leaving the map, then the current forward could become the location of an obstacle
-    unimplemented!("input file: {:?}", input)
+    let map = <Map as TryFrom<&Path>>::try_from(input)?;
+    let mut guard = None;
+    for (position, tile) in map.iter() {
+        if *tile == Tile::Initial {
+            guard = Some(Guard::new(position));
+            break;
+        }
+    }
+    let mut guard = guard.ok_or(Error::GuardNotFound)?;
+    let mut visited = Visited::new(map.width(), map.height());
+
+    let mut obstacle_insertion_positions = HashSet::new();
+
+    while map.in_bounds(guard.position) {
+        visited[guard.position].set(guard.orientation);
+        let forward = guard.position + guard.orientation;
+
+        if obstacle_ahead_succeeds(&guard, &map, &visited) {
+            obstacle_insertion_positions.insert(forward);
+        }
+
+        if map.in_bounds(forward) && map[forward] == Tile::Obstruction {
+            guard.orientation = guard.orientation.turn_right();
+        } else {
+            guard.position = forward;
+        }
+    }
+
+    let new_obstacles = obstacle_insertion_positions.len();
+    println!("potential new obstacles: {new_obstacles}");
+
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
