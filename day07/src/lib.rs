07a65caec16d3966d38b2bf871659d7602754375
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2024-12-08 00:34:01 +0100

    day07: implement part 2
    Well, as you can see from the test cases, I did not get the concat
    implementation correct on the first try. Oh well, it's done.

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -23,6 +23,51 @@ struct Equation {
     values: Values,
 }
 
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
+enum Operation {
+    #[default]
+    Add,
+    Mul,
+    Concat,
+}
+
+impl Operation {
+    /// Continue to the next operation in sequence.
+    ///
+    /// Return `true` if this has overflowed back to the initial state.
+    fn increment(&mut self) -> bool {
+        *self = match self {
+            Operation::Add => Operation::Mul,
+            Operation::Mul => Operation::Concat,
+            Operation::Concat => Operation::Add,
+        };
+        *self == Self::Add
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Operations(Box<[Operation]>);
+
+impl Operations {
+    fn new(dimension: usize) -> Self {
+        Self(vec![Operation::default(); dimension].into())
+    }
+
+    /// Continue to the next operation in sequence.
+    ///
+    /// Return `true` if this has overflowed back to the initial state.
+    fn increment(&mut self) -> bool {
+        let mut idx = 0;
+        let mut incr = true;
+        while incr && idx < self.0.len() {
+            incr = self.0[idx].increment();
+            idx += 1;
+        }
+        debug_assert_eq!(incr, *self == Self::new(self.0.len()));
+        incr
+    }
+}
+
 impl Equation {
     /// Produce all valid operand maps for this equation's values
     fn operand_maps(&self) -> impl Iterator<Item = u32> {
@@ -49,9 +94,44 @@ impl Equation {
         Some(value)
     }
 
+    /// Evaluate the values of this equation according to the supplied operands list.
+    fn evaluate_operands(&self, operands: &Operations) -> Option<i64> {
+        debug_assert_eq!(
+            operands.0.len(),
+            self.values.0.len() - 1,
+            "operands must interleave between equation terms"
+        );
+        let mut value = self.values.0[0];
+        for (operand, v) in operands.0.iter().zip(self.values.0.iter().skip(1)) {
+            match *operand {
+                Operation::Add => value = value.checked_add(*v)?,
+                Operation::Mul => value = value.checked_mul(*v)?,
+                Operation::Concat => {
+                    let n_digits = v.ilog10() + 1;
+                    value = value.checked_mul(10_u32.pow(n_digits) as i64)?;
+                    value = value.checked_add(*v)?;
+                }
+            }
+        }
+        Some(value)
+    }
+
     fn can_evaluate_true(&self) -> bool {
         self.operand_maps()
             .any(|map| self.evaluate(map) == Some(self.test_value))
+    }
+
+    fn can_evaluate_true_with_concat(&self) -> bool {
+        let mut operands = Operations::new(self.values.0.len() - 1);
+        let mut overflowed = false;
+
+        while !overflowed {
+            if self.evaluate_operands(&operands) == Some(self.test_value) {
+                return true;
+            }
+            overflowed = operands.increment();
+        }
+        false
     }
 }
 
@@ -65,7 +145,12 @@ pub fn part1(input: &Path) -> Result<(), Error> {
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    unimplemented!("input file: {:?}", input)
+    let calibration = parse::<Equation>(input)?
+        .filter(Equation::can_evaluate_true_with_concat)
+        .map(|equation| equation.test_value)
+        .sum::<i64>();
+    println!("sum of calibration with concat: {calibration}");
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -76,4 +161,44 @@ pub enum Error {
     NaN(#[from] std::num::ParseIntError),
     #[error("no solution found")]
     NoSolution,
+}
+
+#[cfg(test)]
+mod tests {
+    mod part2 {
+        use crate::*;
+        use rstest::rstest;
+
+        #[rstest]
+        #[case(190, &[10, 19], true)]
+        #[case(3267, &[81, 40, 27], true)]
+        #[case(83, &[17, 5], false)]
+        #[case(156, &[15, 6], true)]
+        #[case(7290, &[6, 8, 6, 15], true)]
+        #[case(161011, &[16, 10, 13], false)]
+        #[case(192, &[17, 8, 14], true)]
+        #[case(21037, &[9, 7, 18, 13], false)]
+        #[case(292, &[11, 6, 16, 20], true)]
+        fn examples(#[case] test_value: i64, #[case] values: &[i64], #[case] expect: bool) {
+            let equation = Equation {
+                test_value,
+                values: Values(values.into()),
+            };
+            assert_eq!(equation.can_evaluate_true_with_concat(), expect);
+        }
+
+        #[rstest]
+        #[case(156, &[15, 6])]
+        #[case(159, &[15, 9])]
+        #[case(1510, &[15, 10])]
+        #[case(1511, &[15, 11])]
+        fn concat(#[case] test_value: i64, #[case] values: &[i64]) {
+            let equation = Equation {
+                test_value,
+                values: Values(values.into()),
+            };
+            let operations = Operations([Operation::Concat].into());
+            assert_eq!(equation.evaluate_operands(&operations), Some(test_value));
+        }
+    }
 }
