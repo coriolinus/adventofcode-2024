975c8617f54a79b7df60dd82a6609df602905dc2
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2024-12-09 08:52:35 +0100

    implement day08 pt 2

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -48,7 +48,41 @@ pub fn part1(input: &Path) -> Result<(), Error> {
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    unimplemented!("input file: {:?}", input)
+    let map = <Map as TryFrom<&Path>>::try_from(input)?;
+    let mut antennae_by_frequency: HashMap<char, Vec<Point>> = Default::default();
+    for (location, ch) in map.iter() {
+        let ch = char::from(*ch);
+        if ch.is_alphanumeric() {
+            antennae_by_frequency.entry(ch).or_default().push(location);
+        }
+    }
+
+    let mut antinodes = HashSet::new();
+    for (_ch, points) in antennae_by_frequency.iter() {
+        for a in points.iter().copied() {
+            for b in points.iter().copied() {
+                if a == b {
+                    continue;
+                }
+
+                let diff = b - a;
+
+                let mut antinode = a;
+                while map.in_bounds(antinode) {
+                    antinodes.insert(antinode);
+                    antinode -= diff;
+                }
+                antinode = b;
+                while map.in_bounds(antinode) {
+                    antinodes.insert(antinode);
+                    antinode += diff;
+                }
+            }
+        }
+    }
+
+    println!("antinodes in map: {}", antinodes.len());
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
