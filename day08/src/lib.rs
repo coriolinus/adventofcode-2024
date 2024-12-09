34be3a812748d229983a955f62dcb85f281beee6
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2024-12-09 08:47:32 +0100

    implement day08 pt 1

---
+++ day08/src/lib.rs
@@ -0,0 +1,60 @@
+use std::{
+    collections::{HashMap, HashSet},
+    path::Path,
+};
+
+use aoclib::geometry::{tile::DisplayWidth, Point};
+
+#[derive(Debug, Copy, Clone, derive_more::FromStr, derive_more::Into)]
+pub struct Char(char);
+
+impl DisplayWidth for Char {
+    const DISPLAY_WIDTH: usize = 1;
+}
+
+type Map = aoclib::geometry::map::Map<Char>;
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    let map = <Map as TryFrom<&Path>>::try_from(input)?;
+    let mut antennae_by_frequency: HashMap<char, Vec<Point>> = Default::default();
+    for (location, ch) in map.iter() {
+        let ch = char::from(*ch);
+        if ch.is_alphanumeric() {
+            antennae_by_frequency.entry(ch).or_default().push(location);
+        }
+    }
+
+    let mut antinodes = HashSet::new();
+    for (_ch, points) in antennae_by_frequency.iter() {
+        for a in points.iter().copied() {
+            for b in points.iter().copied() {
+                if a == b {
+                    continue;
+                }
+
+                let diff = b - a;
+
+                for antinode in [a - diff, b + diff] {
+                    if map.in_bounds(antinode) {
+                        antinodes.insert(antinode);
+                    }
+                }
+            }
+        }
+    }
+
+    println!("antinodes in map: {}", antinodes.len());
+    Ok(())
+}
+
+pub fn part2(input: &Path) -> Result<(), Error> {
+    unimplemented!("input file: {:?}", input)
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("no solution found")]
+    NoSolution,
+}
