abf06dc1bacdbc68249de312a0bdc8e3c97bb812
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2024-12-11 16:40:30 +0100

    day11: implement part 2
    Ugh, they completely threw me for a loop with the emphasis on the
    perfectly straight line. "Yeah, that tracks," I thought. "A `Vec`
    is laid out in memory as a perfectly straight line. This is
    definitely how they intended me to solve the problem." Totally
    ignoring how they did not actually ask for anything which relied
    on any kind of linear layout in memory.
    
    I need to get better at thinking. When 30 seconds of thinking did
    not produce the right strategy, 30 seconds of internet did... and
    that was just too easy a shortcut. Not entirely proud of this one.

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -1,5 +1,5 @@
 use color_eyre::Result;
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
 #[derive(
     Debug,
@@ -9,6 +9,7 @@ use std::path::Path;
     Eq,
     PartialOrd,
     Ord,
+    Hash,
     derive_more::Deref,
     derive_more::DerefMut,
     derive_more::FromStr,
@@ -41,50 +42,70 @@ impl Stone {
     }
 }
 
-fn blink(out: impl IntoIterator<Item = Stone> + ExactSizeIterator, into: &mut Vec<Stone>) {
-    // ensure `into` has capacity at least 1.5x `out.len()`
-    let need_capacity = out.len() * 3 / 2 - into.capacity();
-    into.reserve(need_capacity);
+type StoneCounts = HashMap<Stone, u64>;
 
-    for mut stone in out.into_iter() {
+fn blink(out: impl IntoIterator<Item = (Stone, u64)>, into: &mut StoneCounts) {
+    for (mut stone, count) in out.into_iter() {
         let next = stone.apply_rules();
-        into.push(stone);
+        *into.entry(stone).or_default() += count;
         if let Some(next) = next {
-            into.push(next);
+            *into.entry(next).or_default() += count;
         }
     }
 }
 
-pub fn part1(input: &Path) -> Result<()> {
-    let mut stones = std::fs::read_to_string(input)?
+fn parse(input: &Path) -> Result<StoneCounts> {
+    let mut out = StoneCounts::new();
+    for stone in std::fs::read_to_string(input)?
         .split_whitespace()
         .map(|s| s.parse::<Stone>())
-        .collect::<Result<Vec<_>, _>>()?;
-    let mut temp = Vec::new();
+    {
+        *out.entry(stone?).or_default() += 1;
+    }
+    Ok(out)
+}
 
-    for _ in 0..25 {
-        blink(stones.drain(..), &mut temp);
+fn multiblink(mut stones: StoneCounts, n_blinks: u32) {
+    let mut temp = StoneCounts::new();
+
+    for _ in 0..n_blinks {
+        blink(stones.drain(), &mut temp);
         std::mem::swap(&mut stones, &mut temp);
     }
 
-    println!("after 25 blinks: {}", stones.len());
+    println!("after {n_blinks} blinks: {}", stones.values().sum::<u64>());
+}
 
+pub fn part1(input: &Path) -> Result<()> {
+    let stones = parse(input)?;
+    multiblink(stones, 25);
     Ok(())
 }
 
 pub fn part2(input: &Path) -> Result<()> {
-    let mut stones = std::fs::read_to_string(input)?
-        .split_whitespace()
-        .map(|s| s.parse::<Stone>())
-        .collect::<Result<Vec<_>, _>>()?;
-    let mut temp = Vec::new();
-
-    for _ in 0..75 {
-        blink(stones.drain(..), &mut temp);
-        std::mem::swap(&mut stones, &mut temp);
-    }
-
-    println!("after 75 blinks: {}", stones.len());
-
+    let stones = parse(input)?;
+    multiblink(stones, 75);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    mod part1 {
+        use crate::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn short_example() {
+            let input = [0, 1, 10, 99, 999]
+                .into_iter()
+                .map(|d| (Stone(d), 1))
+                .collect::<StoneCounts>();
+            let mut next = StoneCounts::new();
+            blink(input, &mut next);
+
+            next.retain(|_k, v| *v != 0);
+            let expect = maplit::hashmap! {Stone(1) => 2, Stone(2024) => 1, Stone(0) => 1, Stone(9) => 2, Stone(2021976) => 1};
+            assert_eq!(next, expect);
+        }
+    }
 }
