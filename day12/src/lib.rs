42ebaa44115bca2b968c8c00a69a08677d1d7583
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2024-12-12 09:35:20 +0100

    day12: implement part 1

---
+++ day12/src/lib.rs
@@ -0,0 +1,195 @@
+use aoclib::geometry::{tile::DisplayWidth, Direction, Point};
+use color_eyre::Result;
+use std::path::Path;
+
+#[derive(Debug, Clone, Copy, derive_more::FromStr, derive_more::Into)]
+struct Char(char);
+
+impl DisplayWidth for Char {
+    const DISPLAY_WIDTH: usize = 1;
+}
+
+type RawMap = aoclib::geometry::map::Map<Char>;
+type RegionId = u16;
+type RegionMap = aoclib::geometry::Map<RegionId>;
+
+fn create_region_map<Tile>(map: &aoclib::geometry::Map<Tile>) -> RegionMap
+where
+    Tile: Copy + Eq,
+{
+    fn paint_region<TileInner>(
+        map: &aoclib::geometry::Map<TileInner>,
+        region_map: &mut RegionMap,
+        value: TileInner,
+        region_id: RegionId,
+        point: Point,
+    ) where
+        TileInner: Copy + Eq,
+    {
+        if !map.in_bounds(point) || map[point] != value || region_map[point] != 0 {
+            return;
+        }
+        region_map[point] = region_id;
+        for direction in Direction::iter() {
+            paint_region(map, region_map, value, region_id, point + direction);
+        }
+    }
+
+    let mut region_map = RegionMap::new(map.width(), map.height());
+    let mut next_region_id = 1;
+    for (point, tile) in map.iter() {
+        if region_map[point] == 0 {
+            paint_region(map, &mut region_map, *tile, next_region_id, point);
+            next_region_id += 1;
+        }
+    }
+
+    debug_assert!(!region_map.iter().any(|(_point, tile)| *tile == 0));
+
+    region_map
+}
+
+#[derive(Debug)]
+struct RegionGeometry {
+    #[allow(unused)]
+    first_point: Point,
+    area: u32,
+    perimeter: u32,
+}
+
+impl RegionGeometry {
+    fn analyze(region_map: &RegionMap, id: RegionId) -> Option<Self> {
+        let mut first_point = None;
+        let mut area = 0;
+        let mut perimeter = 0;
+
+        for (point, tile) in region_map.iter() {
+            if *tile == id {
+                if first_point.is_none() {
+                    first_point = Some(point);
+                }
+
+                area += 1;
+                perimeter += 4;
+                for direction in [Direction::Right, Direction::Down] {
+                    let adj = point + direction;
+                    if region_map.in_bounds(adj) && region_map[adj] == id {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+
+        Some(Self {
+            first_point: first_point?,
+            area,
+            perimeter,
+        })
+    }
+
+    fn fence_price(&self) -> u32 {
+        self.area * self.perimeter
+    }
+}
+
+pub fn part1(input: &Path) -> Result<()> {
+    let map = <RawMap as TryFrom<&Path>>::try_from(input)?.convert_tile_type::<char>();
+    let region_map = create_region_map(&map);
+    let mut total_fence_price = 0;
+
+    for region_id in 1.. {
+        let Some(geometry) = RegionGeometry::analyze(&region_map, region_id) else {
+            break;
+        };
+
+        total_fence_price += geometry.fence_price();
+    }
+
+    println!("total fence price: {total_fence_price}");
+    Ok(())
+}
+
+pub fn part2(input: &Path) -> Result<()> {
+    unimplemented!("input file: {:?}", input)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    type CMap = aoclib::geometry::Map<char>;
+
+    fn tiny_example() -> CMap {
+        let data = "
+AAAA
+BBCD
+BBCC
+EEEC
+        "
+        .trim();
+        <RawMap as TryFrom<&str>>::try_from(data)
+            .unwrap()
+            .convert_tile_type()
+    }
+
+    fn small_example() -> CMap {
+        let data = "
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+        "
+        .trim();
+        <RawMap as TryFrom<&str>>::try_from(data)
+            .unwrap()
+            .convert_tile_type()
+    }
+
+    fn big_example() -> CMap {
+        let data = "
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+        "
+        .trim();
+        <RawMap as TryFrom<&str>>::try_from(data)
+            .unwrap()
+            .convert_tile_type()
+    }
+
+    mod part1 {
+        use super::*;
+        use rstest::rstest;
+
+        #[rstest]
+        #[case::tiny(tiny_example(), 5, 140)]
+        #[case::small(small_example(), 5, 772)]
+        #[case::big(big_example(), 11, 1930)]
+        fn analyze_regions(
+            #[case] map: CMap,
+            #[case] expect_n_regions: RegionId,
+            #[case] expect_total_price: u32,
+        ) {
+            let region_map = create_region_map(&map);
+            assert!(!region_map.iter().any(|(_point, id)| *id == 0));
+
+            let max_region_id = region_map.iter().map(|(_point, id)| *id).max().unwrap();
+            assert_eq!(max_region_id, expect_n_regions);
+
+            let mut total_price = 0;
+            for id in 1..=max_region_id {
+                let geometry = RegionGeometry::analyze(&region_map, id).unwrap();
+                dbg!(id, &geometry);
+                total_price += geometry.fence_price();
+            }
+            assert_eq!(total_price, expect_total_price);
+        }
+    }
+}
